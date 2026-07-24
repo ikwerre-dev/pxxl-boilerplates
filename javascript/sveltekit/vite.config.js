cb38deb4c98799b09dabb2ab0b77d5ea71766f7b
Robinson Honour <117399956+ikwerre-dev@users.noreply.github.com>
2026-07-24 20:50:53 +0100

    Complete framework starter entrypoints

---
+++ javascript/sveltekit/vite.config.js
@@ -0,0 +1,1 @@
+import { defineConfig } from "vite"; import { sveltekit } from "@sveltejs/kit/vite"; export default defineConfig({ plugins: [sveltekit()] });
---
+++ javascript/tanstack-start/src/router.tsx
@@ -0,0 +1,1 @@
+import { createRouter } from "@tanstack/react-router"; import { routeTree } from "./routeTree.gen"; export function getRouter(){return createRouter({routeTree,scrollRestoration:true});}
