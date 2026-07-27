f3f7f25fcfcfbbd38659a2a9d0f28dd12b3fd772
Robinson Honour <117399956+ikwerre-dev@users.noreply.github.com>
2026-07-27 06:47:38 +0100

    package Ktor fixture as runnable jar

--- jvm/ktor-kotlin/build.gradle.kts
+++ jvm/ktor-kotlin/build.gradle.kts
@@ -2,3 +2,4 @@ plugins { kotlin("jvm") version "2.1.21"; application }
 repositories { mavenCentral() }
 dependencies { implementation("io.ktor:ktor-server-netty:3.1.3"); implementation("io.ktor:ktor-server-content-negotiation:3.1.3"); implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.3") }
 application { mainClass.set("dev.pxxl.ApplicationKt") }
+tasks.jar { manifest { attributes["Main-Class"]="dev.pxxl.ApplicationKt" }; duplicatesStrategy=DuplicatesStrategy.EXCLUDE; from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }) }
